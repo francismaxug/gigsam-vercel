312e4bd0696bac1386a4f4e478bfe2d36a6caf76
francismaxug <fatinga@st.ug.edu.gh>
2025-03-01 14:19:15 +0000

    checked responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import LandingPage from "@/components/hero/hero"
 
 export default function Home() {
   return (
-    <div className="md:mx-auto md:w-[63%] w-full    font-[family-name:var(--font-geist-sans)]">
+    <div className="md:mx-auto md:w-[63%] w-full px-3    font-[family-name:var(--font-geist-sans)]">
       <LandingPage />
     </div>
   )
